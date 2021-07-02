9382522f42f586fc5a07dfb392076039c6c1bc00
ppusda <ppusda1234@gmail.com>
2021-07-02 20:30:14 +0900

    엔티티 설계의 주의사항들, 연관관계 편의 메서드

--- jpashop/src/main/java/com/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/com/jpabook/jpashop/domain/Member.java
@@ -15,7 +15,6 @@ public class Member {
     @Column(name = "member_id")
     private Long Id;
 
-
     private String name;
 
     @Embedded // 내장 타입을 가지고 있다는 뜻
@@ -23,8 +22,8 @@ public class Member {
 
     @OneToMany(mappedBy = "member") // 일대다 관계 명시, Member 와 Order 와의 관계
     private List<Order> orders = new ArrayList<>();
-}
 
+}
 
 // Member는 Orders를 list로 가지고 있다
 // 반대쪽도 마찬가지기에 우리는 연관관계의 주인을 설정해야한다.
--- jpashop/src/main/java/com/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -17,14 +19,15 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne // 다대일 관계 명시, Member 와 Order 와의 관계
+    @ManyToOne(fetch = LAZY) // 다대일 관계 명시, Member 와 Order 와의 관계
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    // 모든 Entity는 저장하고 싶으면 각자 persist를 해주어야 하지만 CascadeType.ALL을 이용하면 Order을 persist 했을 때 같이 묶여서 된다.
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,6 +36,23 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 ORDER, CANCEL을 가짐
 
+    // -- 연관관계 메서드 -- //
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    } // 양방향 연관관계 편의 메서드
+    // 한번에 처리를 위한 메서드, 각각의 곳에 값을 넣기 위해 메소드를 작성
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+    // 양방향일 때 사용
 }
 
 // Order도 Memeber를 가지고 있다.
